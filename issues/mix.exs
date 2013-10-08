a59cd6a6215941b6c322c1a6422f0e853d62ea81
Andreas Eger <dev@eger-andreas.de>
2013-10-08 23:39:04 +0200

    issues: cli

--- issues/mix.exs
+++ issues/mix.exs
@@ -16,6 +16,8 @@ defmodule Issues.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [
+      { :httpotion, github: "myfreeweb/httpotion"}
+    ]
   end
 end
